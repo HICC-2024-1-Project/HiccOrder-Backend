6050bbcc289c1c3faf62f590a9670babd7a54517
JunhoKim <boy814@naver.com>
2024-04-03 14:21:49 +0900

    feat: 로그인, 로그아웃, 정보조회 기능 추가, api 명세서와 url이 달라져 체크 필요

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import RegisterAPIView
+from .views import SignUpAPIView
+from .views import AuthAPIView
 
 urlpatterns = [
-    path("sign-up/", RegisterAPIView.as_view()),   # post-회원 가입
+    path("sign-up", SignUpAPIView.as_view()),   # post-회원 가입
+    path("", AuthAPIView.as_view()),   # post - 로그인, delete - 로그아웃, get - 유저정보
 ]
