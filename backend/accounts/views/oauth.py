709b02b439f7d7aac8f3ab8dc326d1db262bf7df
JunhoKim <boy814@naver.com>
2024-05-13 21:22:02 +0900

    fix: 이미 이메일이 존재하는 경우 로그인

--- backend/accounts/views/oauth.py
+++ backend/accounts/views/oauth.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate
 from ..models import *
 from rest_framework.views import APIView
-from ..serializers import UserSerializerWithNoPassword
+from ..serializers import UserSerializerWithNoPassword, UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.response import Response
 import os
@@ -61,32 +62,45 @@ class GoogleCallbackAPIView(APIView):
 
         #################################################################
 
-        # 3. 가져온 이메일로 회원 가입 진행
-        data = {"email": email, "password": '', "is_oauth": True}
-        print(data)
-        serializer = UserSerializerWithNoPassword(data=data)
-        if serializer.is_valid():
-            user = serializer.save()
-
-            # jwt 토큰 접근
+        user = User.objects.get(email=email)
+        if user is not None and user.is_oauth:
+            # 이미 회원이 존재하는 경우, 로그인을 수행하고 토큰을 발급합니다.
             token = TokenObtainPairSerializer.get_token(user)
             refresh_token = str(token)
             access_token = str(token.access_token)
-            res = Response(
-                {
+            response_data = {
+                "user": UserSerializer(user).data,
+                "message": "login success",
+                "token": {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                },
+            }
+            response = Response(response_data, status=status.HTTP_200_OK)
+            response.set_cookie("access", access_token, httponly=True)
+            response.set_cookie("refresh", refresh_token, httponly=True)
+            return response
+        else:
+            # 새로운 회원 가입을 진행합니다.
+            data = {"email": email, "password": ''}
+            serializer = UserSerializerWithNoPassword(data=data)
+            if serializer.is_valid():
+                user = serializer.save()
+
+                # jwt 토큰 접근
+                token = TokenObtainPairSerializer.get_token(user)
+                refresh_token = str(token)
+                access_token = str(token.access_token)
+                response_data = {
                     "user": serializer.data,
-                    "message": "register successs",
+                    "message": "register success",
                     "token": {
                         "access": access_token,
                         "refresh": refresh_token,
                     },
-                },
-                status=status.HTTP_200_OK,
-            )
-
-            # jwt 토큰 => 쿠키에 저장
-            res.set_cookie("access", access_token, httponly=True)
-            res.set_cookie("refresh", refresh_token, httponly=True)
-
-            return res
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                }
+                response = Response(response_data, status=status.HTTP_200_OK)
+                response.set_cookie("access", access_token, httponly=True)
+                response.set_cookie("refresh", refresh_token, httponly=True)
+                return response
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
