4fbf4a1e25130b53d592df8f0ec826972bd29ea7
Jang hyunhak <gusgkr785@gmail.com>
2024-08-10 17:28:56 +0900

    s3 세팅 설정

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -245,3 +245,10 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+# S3 setting
+AWS_ACCESS_KEY_ID = get_secret("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_secret("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'hiccorder2')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-2')
+IMAGE_URL = "https://%s.s3.%s.amazonaws.com/" % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
