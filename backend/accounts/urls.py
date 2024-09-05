590cee61eb3287916c439ae8b85e3bffd82c4d46
JunhoKim <boy814@naver.com>
2024-09-05 10:22:40 +0900

    chore: url 순서 의미 단위에 맞게 수정

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     path("auth/", AuthAPIView.as_view()),   # post-로그인, delete-로그아웃, get-유저정보
     path("auth/duplicate/", EmailDuplication.as_view()),  # post-이메일 중복 확인
     path("auth/refresh/", RefreshView.as_view(), name='token_refresh'),     # post-refresh token 재발급
+    path("auth/google/login/", google_login, name='google_login'),
+    path("auth/google/callback/", GoogleCallbackAPIView.as_view(), name='google_callback'),
+    path("auth/password/", SendVerificationCodeView.as_view()),
+    path("auth/password/verify/", VerifyCodeView.as_view()),
+    path("auth/qrlink/", GenerateTemporaryLinkAPIView.as_view()),
+    path("auth/qrsignin/<str:token>/", TemporaryResourceAPIView.as_view()),
     path("booth/order/", TableOrderAPIView.as_view()),  # 사용자용 테이블 주문 및 주문 확인
     path("booth/<str:booth_id>/order/", BoothOrderAPIView.as_view()),   # 전체 주문 목록 확인
     path("booth/<str:booth_id>/order/<str:table_id>/", TableOrderManagerAPIView.as_view()),  # 관리자용 테이블 주문 및 주문 확인
@@ -18,12 +24,6 @@ urlpatterns = [
     path("booth/<str:booth_id>/payment/<str:table_id>/", OrderPaymentAPIView.as_view()),    # 결제
     path("booth/<str:booth_id>/", BoothAPIView.as_view()),
     path("booth/<str:booth_id>/menu/", BoothMenuAPIView.as_view()),
-    path("auth/google/login/", google_login, name='google_login'),
-    path("auth/google/callback/", GoogleCallbackAPIView.as_view(), name='google_callback'),
-    path("auth/password/", SendVerificationCodeView.as_view()),
-    path("auth/password/verify/", VerifyCodeView.as_view()),
-    path("auth/qrlink/", GenerateTemporaryLinkAPIView.as_view()),
-    path("auth/qrsignin/<str:token>/", TemporaryResourceAPIView.as_view()),
     path("booth/<str:booth_id>/menu/<str:menu_id>/", BoothMenuDetailAPIView.as_view()),
     path("booth/<str:booth_id>/table/", TableAPIView.as_view()),
     path("booth/<str:booth_id>/table/<str:table_id>/", TableDetailAPIVIew.as_view()),
