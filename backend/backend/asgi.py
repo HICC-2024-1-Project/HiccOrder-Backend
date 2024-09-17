c88464880532b70ea2ffad83c92427b60bd80b7a
Junho Kim <boy814@naver.com>
2024-09-17 18:53:10 +0900

    fix: asgi import 순서 및 실행 코드 위치 수정

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -11,13 +11,15 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from accounts import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
