2d1fd6a641eb9c5beaacd3b8698fedf5f48ac107
Junho Kim <boy814@naver.com>
2024-09-18 14:40:26 +0900

    feat: admin url과 user url 분리

--- backend/accounts/routing.py
+++ backend/accounts/routing.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .consumers.staffcall import StaffCallConsumer
 
 websocket_urlpatterns = [
+    path('ws/<str:type>/<str:booth_id>/', StaffCallConsumer.as_asgi()),
     path('ws/<str:type>/<str:booth_id>/<str:table_id>/', StaffCallConsumer.as_asgi()),
 ]
