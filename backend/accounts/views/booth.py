58ea573fa778428d0ed19844c69250c7f6ee1495
JunhoKim <boy814@naver.com>
2024-09-26 15:01:48 +0900

    fix: table_fee에서 booth_id 오타 수정

--- backend/accounts/views/booth.py
+++ backend/accounts/views/booth.py
@@ -628,8 +628,8 @@ class TableFeeAPIView(APIView):
         if not check_authority(request, booth_id):
             return Response({"message": "권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        times = Time.objects.filter(email_id=booth_id)
+        times = Time.objects.filter(booth_id=booth_id)
 
         serializer = TimeSerializer(times, many=True)
 
-        return Response (serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
